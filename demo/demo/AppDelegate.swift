d9a7dd8f76eac3f89265c3119fc55fc81f15b3c6
czw <changzhongwei3@163.com>
2022-09-09 16:40:11 +0800

    init

---
+++ demo/demo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  demo
+//
+//  Created by Fri on 2022/9/9.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+    return true
+  }
+  
+}
+
---
+++ demo/demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Fri on 2022/9/9.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .magenta
+    
+  }
+
+}
+
